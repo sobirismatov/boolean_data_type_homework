ec1299033bc442205a582d2234f4de14ad4c8648
ikromyusupov <name@ikromyusupovdev@gmail.com>
2022-10-24 17:02:58 +0500

    done

---
+++ bool01.py
@@ -0,0 +1,11 @@
+def main(a,b):
+    """
+    check the following statement "they are equal"
+    Args:
+        a: int
+        b: int
+    Returns:
+        bool
+    """
+    # Write your code here
+    return 
